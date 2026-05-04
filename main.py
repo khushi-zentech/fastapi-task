18d6baf1750974bc942479bb78070214e64ab265
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-05-04 15:43:48 +0530

    [ Add ]: Cover topic of request body

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    total_price: float
 
 app = FastAPI()
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: str | None = None):
-#     if q:
-#         return {"item_id": item_id, "q": q}
-#     return {"item_id": item_id}
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: str | None = None, short: bool = False):
-#     item = {"item_id": item_id}
-#     if q:
-#         item.update({"q": q})
+@app.post("/items/")
+async def create_item(item: Item):
+    if item.tax:
+        item.total_price = item.price + item.tax
+    else:
+        item.total_price = item.price
     
-#     if not short:
-#         item.update({"description": "This is an amazing item that has a long description"})
-#     return item
-
-# @app.get("/items/{item_id}")
-# async def read_user_item(item_id: str, needy: str):
-#     return {"item_id": item_id, "needy": needy}
-
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
-    return {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    
-    if not short:
-        item.update({"description": "This is an amazing item that has a long description"})
     return item
+
+@app.post("/items/add_item/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    
+    if item.tax is not None:
+        item.total_price = item.price + item.tax
+        item_dict.update({"total_price_with_tax": item.total_price})
+    
+    return item_dict
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.model_dump()}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    
+    if q:
+        result.update({"q": q})
+    return result
